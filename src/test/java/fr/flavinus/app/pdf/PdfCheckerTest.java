7d126fac11a8578ae14286ced4236759927193f3
fletestu <flavien.letestu@ubikasec.com>
2022-11-16 14:52:41 +0100

    test: add unit tests on files

--- src/test/java/fr/flavinus/app/pdf/PdfCheckerTest.java
+++ src/test/java/fr/flavinus/app/pdf/PdfCheckerTest.java
@@ -3,7 +3,14 @@ package fr.flavinus.app.pdf;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+
 import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+
 import org.junit.Test;
 import fr.flavinus.pdf.PdfChecker;
 
@@ -34,13 +41,40 @@ public class PdfCheckerTest {
     }
 
     @Test
-    public void testPdfCheckerRun() {
-        pdfCheckerExecute("not a pdf", -1);
-        pdfCheckerExecute("%PDF-1.4% \nincomplete content", -1);
-        pdfCheckerExecute("%PDF-1.4% \ncontent\n%%EOF", 19);        
+    public void testPdfCheckerRunString() {
+        pdfCheckerTestString("not a pdf", -1);
+        pdfCheckerTestString("%PDF-1.4% \nincomplete content", -1);
+        pdfCheckerTestString("%PDF-1.4% \ncontent\n%%EOF", 19);        
     }
 
-    private void pdfCheckerExecute(String input, long offset) {
+    private void pdfCheckerTestString(String input, long offset) {
         assertEquals(offset, PdfChecker.execute(new ByteArrayInputStream(input.getBytes())));
+    }
+
+    @Test
+    public void testPdfCheckerRunFile() {
+
+        pdfCheckerTestFile("samples/minimal-pdf-sample.pdf", 8422);
+        pdfCheckerTestFile("README.md", -1);
+
+        /* Waiting for commit confirmation
+        pdfCheckerTestFile("samples/test_1.pdf", 4568);
+        pdfCheckerTestFile("samples/test_2.pdf", 22195);
+        pdfCheckerTestFile("samples/test_3.pdf", 19155);
+        pdfCheckerTestFile("samples/test_4.pdf", 19155);
+        pdfCheckerTestFile("samples/test_5.pdf", -1);
+        pdfCheckerTestFile("samples/test_6.pdf", 18835);
+        pdfCheckerTestFile("samples/test_7.pdf", 22145);
+        pdfCheckerTestFile("samples/test_8.pdf", 22139);
+        */
+    }
+
+    private void pdfCheckerTestFile(String filename, long offset) {
+        InputStream inputStream = null;
+        try {
+            inputStream = new FileInputStream(new File(filename));
+        } catch (FileNotFoundException e) {}
+        assertNotNull("Failed to load test file '" + filename + "'", inputStream);
+        assertEquals("Failed pdfChecker execute on file '" + filename + "'", offset, PdfChecker.execute(inputStream));
     }
 }
